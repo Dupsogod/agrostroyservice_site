b477013d8f1a1a002b6a7691e0c6263a8825da9e
Dupsogod <dupsogod@gmail.com>
2020-02-16 20:43:27 +0300

    calculator: step_3

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,4 @@
-//sending the selected region to the form
+//sending the selected to the form
 
 $('#Rostov').click(function(){
 	$('input[name="region__name"][value="Rostov"]').prop('checked', true);
@@ -84,6 +84,58 @@ $('#Tambov').click(function(){
 	$('input[name="region__name"][value="Tambov"]').prop('checked', true);
 });
 
+$('#agricultural').click(function(){
+	$('input[name="type-of-building"][value="agricultural"]').prop('checked', true);
+});
+
+$('#industry').click(function(){
+	$('input[name="type-of-building"][value="industry"]').prop('checked', true);
+});
+
+$('#office').click(function(){
+	$('input[name="type-of-building"][value="office"]').prop('checked', true);
+});
+
+$('#office').click(function(){
+	$('input[name="type-of-building"][value="office"]').prop('checked', true);
+});
+
+$('#granary').click(function(){
+	$('input[name="type-of-building-2"][value="granary"]').prop('checked', true);
+});
+
+$('#cowshed').click(function(){
+	$('input[name="type-of-building-2"][value="cowshed"]').prop('checked', true);
+});
+
+$('#henhouse').click(function(){
+	$('input[name="type-of-building-2"][value="henhouse"]').prop('checked', true);
+});
+
+$('#storage').click(function(){
+	$('input[name="type-of-building-2"][value="storage"]').prop('checked', true);
+});
+
+$('#hangar').click(function(){
+	$('input[name="type-of-building-2"][value="hangar"]').prop('checked', true);
+});
+
+$('#factory').click(function(){
+	$('input[name="type-of-building-2"][value="factory"]').prop('checked', true);
+});
+
+$('#other-industrial').click(function(){
+	$('input[name="type-of-building-2"][value="other-industrial"]').prop('checked', true);
+});
+
+$('#office-building').click(function(){
+	$('input[name="type-of-building-2"][value="office-building"]').prop('checked', true);
+});
+
+$('#shop').click(function(){
+	$('input[name="type-of-building-2"][value="shop"]').prop('checked', true);
+});
+
 //modal window with form
 
 $(document).ready(function($) {
@@ -114,10 +166,75 @@ $(document).ready(function($) {
 //tabs
 (function ($) {
 	$(function () {
-		$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+		$('nav.tabs__caption').on('click', '.caption-elem:not(.active)', function () {
 			$(this).addClass('active').siblings().removeClass('active')
 				.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 		})
 	})
 })(jQuery)
+
+//add active class on form buttons
+
+$('.step-form__radio-button').click(function () {
+	if (!$(this).hasClass('step-form__radio-button--active')) {
+		$(this).siblings().removeClass('step-form__radio-button--active');
+		$(this).addClass('step-form__radio-button--active');
+	}
+});
+
+//close or open windows inside forms
+
+$('#agricultural').click(function() {
+	$('#agricultural-form').addClass("open");
+	$('#industry-form').removeClass("open");
+	$('#office-form').removeClass("open");
+});
+
+$('#industry').click(function() {
+	$('#agricultural-form').removeClass("open");
+	$('#industry-form').addClass("open");
+	$('#office-form').removeClass("open");
+});
+
+$('#office').click(function() {
+	$('#agricultural-form').removeClass("open");
+	$('#industry-form').removeClass("open");
+	$('#office-form').addClass("open");
+});
+
+
+// range slider
+
+$('.input-block div ').hover(function(){
+	var idPer=$(this).children('.input-block__item').attr('Id')
+
+	var $rangeslider =$('#'+ idPer)
+	var $amount = $('#'+$(this).children('.input-block__item').siblings('input').attr('Id'))
+
+	$rangeslider.on('input', function() {
+		$amount.val(this.value).change() 
+	});
+
+	$amount.on('input', function() {
+		$rangeslider.val(this.value).change(); 
+	});
+});
+
+$('.input-block div input[type="range"]').rangeslider({
+	polyfill: false
+})
+
+
+$('.map g').click(function () {
+	var clickId = this.id;
+	alert(clickId);
+	var rad = document.getElementsByName('region__name');
+	console.log(rad);
+	var propID = $('input[name="region__name"]').val();
+	alert(propID);
+});
+
+
+
+
 
